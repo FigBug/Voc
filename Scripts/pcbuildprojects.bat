b498b12e3ddf28773f621f47787a8acb00d6c2a5
Roland Rabien <figbug@gmail.com>
2019-11-22 09:20:20 -0800

    Compress results

--- Scripts/pcbuildprojects.bat
+++ Scripts/pcbuildprojects.bat
@@ -7,7 +7,6 @@ cd "%ROOT%"
 cd Scripts
 mkdir bin
 mkdir bin\win
-mkdir bin\zip
 
 cd "..\plugin\Builds\VisualStudio2017"
 "%MSBUILD_EXE%" Voc.sln /p:VisualStudioVersion=15.0 /m /t:Build /p:Configuration=Release64 /p:Platform=x64 /p:PreferredToolArchitecture=x64
@@ -17,6 +16,9 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 cd "%ROOT%\Scripts\bin"
 
-copy "%ROOT%\plugin\Builds\VisualStudio2017\x64\Release64\VST\Voc_64b.dll" zip
-copy "%ROOT%\plugin\Builds\VisualStudio2017\Win32\Release\VST\Voc_32b.dll" zip
+copy "%ROOT%\plugin\Builds\VisualStudio2017\x64\Release64\VST\Voc_64b.dll" . 
+copy "%ROOT%\plugin\Builds\VisualStudio2017\Win32\Release\VST\Voc_32b.dll" .
+
+7z a Voc_Win.zip Voc_32b.dll Voc_64b.dll
+copy Voc_Win.zip win
 
